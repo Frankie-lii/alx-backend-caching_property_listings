9296b44c8d9839e21effb622868334365670d1ee
Frankie-lii <oyiengofranklne49@gmail.com>
2025-08-30 00:31:58 -0700

    task 3

---
+++ properties/apps.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from django.apps import AppConfig
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'properties'
+
+    def ready(self):
+        import properties.signals  # Ensures signals are loaded
+
